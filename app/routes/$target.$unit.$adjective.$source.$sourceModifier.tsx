67b1885e7ad0bcb7eac0cb4e096a63140f695b7a
Kory Smith <kor54e@gmail.com>
2022-08-10 21:13:12 -0400

    adds a first draft of the main (and likely only) page

---
+++ app/routes/$target.$unit.$adjective.$source.$sourceModifier.tsx
@@ -0,0 +1,34 @@
+import { json, LoaderArgs } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import { useLoaderData } from "@remix-run/react";
+import { DateTime } from "luxon";
+
+export const loader = ({ params }: LoaderArgs) => {
+  const { target, unit, adjective, source, sourceModifier } = params;
+
+  const trueSource = DateTime.fromFormat(`${source} ${sourceModifier}`, "t");
+
+  invariant(unit, "must be existing");
+
+  const plusOrMinus = "plus";
+
+  const solution = trueSource[plusOrMinus]({ [unit]: target });
+
+  return json({
+    source: `What time will it be ${target} ${unit} ${adjective} ${trueSource.toLocaleString(
+      DateTime.TIME_SIMPLE
+    )}?`,
+    solution: solution.toLocaleString(DateTime.TIME_SIMPLE),
+  });
+};
+
+export default function Example() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <>
+      <h1>{data.source}</h1>
+      <br />
+      <h2>It will be {data.solution}</h2>
+    </>
+  );
+}
