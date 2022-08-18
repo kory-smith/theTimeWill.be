8bed6386045e49db5a931889ad3b5f7fbd796aa6
Kory Smith <kor54e@gmail.com>
2022-08-17 20:22:55 -0400

    cleans up the 24-hour route

--- app/routes/$target.$unit.$adjective.$source/index.tsx
+++ app/routes/$target.$unit.$adjective.$source/index.tsx
@@ -8,52 +8,35 @@ import { getTimeFormat } from "~/helpers/timeFormatting";
 // https://theTimeWill.be/122/minutes/before/13:50/
 // https://theTimeWill.be/122/minutes/before/1350/
 
-
-const t = /^(\d{1,2})(:)(\d{1,2})(\ )(AM|PM)$/i
-const T = /^(\d{1,2})(:)(\d{1,2})$/i
-
-function getTimeFormat(params: Params) {
-  const { target, unit, adjective, source, meridian } = params;
-
-//  console.log( DateTime.fromFormatExplain(`${source} ${meridian}`, "T"))
- console.log( t.test(`${source} ${meridian}`))
-
-
-
-}
-
 export const loader = ({ params }: LoaderArgs) => {
   //                       target | unit  | adjective | source | meridian
   //                        ▼▼▼▼▼▼ ▼▼▼▼▼▼▼ ▼▼▼▼▼▼▼▼▼▼▼ ▼▼▼▼▼▼▼▼  ▼▼▼▼▼▼▼▼▼▼
   // https://theTimeWill.be/122   /minutes/before     /7:50    /pm
   const { target, unit, adjective, source } = params;
 
-  getTimeFormat(params)
+  const { parsingKey, formatKey } = getTimeFormat(params);
 
- /*  
- What types of times can we have?
- 24 hour time - 2130
- 7/pm
- 07/pm
- 7:56/pm
- 07:56/pm
- */
-  // https://moment.github.io/luxon/#/parsing?id=table-of-tokens
-  const trueSource = DateTime.fromFormat(`${source}`, "T");
-
+  // todo: Check if we are in 24-hour time but in the meridian route. If so, change things.
+  // Is that possible?
+  // if (parsingKey) {
+  //   return redirect(`/${target}/${unit}/${adjective}/${source}/`)
+  // }
 
 
   invariant(unit, "must be existing");
+  invariant(source, "must be existing");
+  
+  const trueSource = DateTime.fromFormat(source.trim(), parsingKey);
 
-  const plusOrMinus = adjective === "past" ? "plus" : "minus"
+  const plusOrMinus = adjective === "past" ? "plus" : "minus";
 
   const solution = trueSource[plusOrMinus]({ [unit]: target });
 
   return json({
-    source: `What time will it be ${target} ${unit} ${adjective} ${trueSource.toLocaleString(
-      DateTime.TIME_SIMPLE
+    source: `What time will it be ${target} ${unit} ${adjective} ${trueSource.toFormat(
+      parsingKey
     )}?`,
-    solution: solution.toLocaleString(DateTime.TIME_SIMPLE),
+    solution: solution.toLocaleString(formatKey),
   });
 };
 
