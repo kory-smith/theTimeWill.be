047d6e5deca91dbc9945cf441eff1629144aa4f6
Kory Smith <kor54e@gmail.com>
2022-11-09 19:38:42 -0500

    adds a clipboard icon (currently unclickable) to the result time

--- app/routes/$target.$unit.$adjective.$source.$meridiem.tsx
+++ app/routes/$target.$unit.$adjective.$source.$meridiem.tsx
@@ -71,16 +71,33 @@ export const loader = ({ params, request}: LoaderArgs) => {
 };
 
 export default function Example() {
-  const data = useLoaderData<typeof loader>();
-  return (
-    <>
-      <h1>{data.source}</h1>
-      <h2>
-        It will be <time>{data.solution}</time>
-        {data.offset ? ` ${data.offset}` : null}
-      </h2>
-    </>
-  );
+	const data = useLoaderData<typeof loader>();
+	return (
+		<>
+			<h1 className="text-3xl">{data.source}</h1>
+			<h2 className="font-kory text-fucko inline">
+				It will be <time className="text-blue-500">{data.solution}</time>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					className="w-14 h-14 inline"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M15.988 3.012A2.25 2.25 0 0118 5.25v6.5A2.25 2.25 0 0115.75 14H13.5V7A2.5 2.5 0 0011 4.5H8.128a2.252 2.252 0 011.884-1.488A2.25 2.25 0 0112.25 1h1.5a2.25 2.25 0 012.238 2.012zM11.5 3.25a.75.75 0 01.75-.75h1.5a.75.75 0 01.75.75v.25h-3v-.25z"
+						clip-rule="evenodd"
+					/>
+					<path
+						fill-rule="evenodd"
+						d="M2 7a1 1 0 011-1h8a1 1 0 011 1v10a1 1 0 01-1 1H3a1 1 0 01-1-1V7zm2 3.25a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75zm0 3.5a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+				{data.offset ? ` ${data.offset}` : null}
+			</h2>
+		</>
+	);
 }
 
 export function CatchBoundary() {
