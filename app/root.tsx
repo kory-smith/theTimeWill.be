63e37a325f35ec378ae64e4b97068b56b8cd7da6
Kory Smith <kor54e@gmail.com>
2022-12-21 15:19:15 -0500

    makes the button blue!

--- app/root.tsx
+++ app/root.tsx
@@ -108,7 +108,7 @@ export default function App() {
 
               <button
                 type="submit"
-                className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="inline-flex items-center rounded-md border border-gray-300 bg-blue-500 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 Go
               </button>
