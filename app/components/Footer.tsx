74a686db135eac956ec2140fb281dab7a8b18fdf
Kory Smith <kory.smith@kroger.com>
2022-12-22 13:48:33 -0500

    breaks out components into their own files

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+export function Footer() {
+	return (
+		<footer className="bg-black text-white text-center py-4">
+			Made with ❤️️ by{" "}
+			<a
+				href="https://github.com/kory-smith"
+				className="underline"
+			>
+				Kory Smith
+			</a>
+		</footer>
+	);
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,9 @@
 import { withZod } from "@remix-validated-form/with-zod";
-import { ValidatedForm, validationError } from "remix-validated-form";
+import { validationError } from "remix-validated-form";
 import { z } from "zod";
 import { ActionArgs, MetaFunction, redirect } from "@remix-run/node";
+import { Footer } from "./components/Footer";
+import { TimeForm } from "./components/TimeForm";
 import {
 	Links,
 	LiveReload,
@@ -42,20 +44,6 @@ export const links = () => {
 	return [{ rel: "stylesheet", href: styles }];
 };
 
-function Footer() {
-	return (
-		<footer className="bg-black text-white text-center py-4">
-			Made with ❤️️ by{" "}
-			<a
-				href="https://github.com/kory-smith"
-				className="underline"
-			>
-				Kory Smith
-			</a>
-		</footer>
-	);
-}
-
 export default function App() {
 	return (
     <html lang="en">
@@ -65,58 +53,10 @@ export default function App() {
       </head>
       <body className="font-avenir flex flex-col min-h-screen">
         <main className="m-16 flex-grow">
-          <ValidatedForm validator={validator} resetAfterSubmit method="post">
-            <h2 className="text-3xl">What time will it be...</h2>
-
-            <div className="my-4">
-              <input
-                type="number"
-                name="target"
-                className="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm inline-block text-sm font-medium text-gray-700 mx-8"
-                placeholder="122"
-              />
-
-              <input
-                list="units"
-                autoComplete="on"
-                name="unit"
-                placeholder="minute(s)"
-                className="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm inline-block text-sm font-medium text-gray-700 mx-8"
-              />
-              <datalist id="units">
-                <option value="second(s)" />
-                <option value="minute(s)" />
-                <option value="hour(s)" />
-              </datalist>
-
-              <input
-                list="adjectives"
-                name="adjective"
-                placeholder="before"
-                className="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm inline-block text-sm font-medium text-gray-700"
-              />
-              <datalist id="adjectives">
-                <option value="before" />
-                <option value="after" />
-              </datalist>
-
-              <input
-                type="time"
-                name="source"
-                className="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm inline-block text-sm font-medium text-gray-700 mx-8"
-              />
-
-              <button
-                type="submit"
-                className="inline-flex items-center rounded-md border border-gray-300 bg-blue-500 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Go
-              </button>
-            </div>
-          </ValidatedForm>
+          <TimeForm validator={validator} />
           <hr className="m-2" />
           <Outlet />
-        </div>
+        </main>
         <Footer />
         <ScrollRestoration />
         <Scripts />
