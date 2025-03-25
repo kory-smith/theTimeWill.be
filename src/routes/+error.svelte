95830d92a116f0de7597d40b85a03126e4a8f0b1
Kory Smith <kory.smith@kroger.com>
2025-03-25 11:42:18 -0400

    adds error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<p>{page?.error?.message}</p>
+<p>That's not a valid URL! Try <a href="/12/minutes/after/2:35/pm">12 minutes after 2:35 pm</a></p>
